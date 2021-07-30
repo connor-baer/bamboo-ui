40ca733833bb502db37120973ee0e7fc13262e60
Connor Bär <connor.baer@me.com>
2021-07-30 10:21:38 +0200

    Migrate Input component

---
+++ src/components/forms/Input/Input.story.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { Input, InputProps } from './Input';
+
+export default {
+  title: 'Forms/Input',
+  component: Input,
+};
+
+export const Base = (args: InputProps) => <Input {...args} />;
+
+Base.args = {
+  label: 'What is your name?',
+  placeholder: 'Panda',
+  onChange: action('Input changed'),
+};
