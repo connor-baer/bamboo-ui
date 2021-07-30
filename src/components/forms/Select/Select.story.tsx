c63ba579212dcd6cefba99c503291450f0be6849
Connor Bär <connor.baer@me.com>
2021-07-30 10:22:21 +0200

    Migrate Select component

---
+++ src/components/forms/Select/Select.story.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { Select, SelectProps } from './Select';
+
+export default {
+  title: 'Forms/Select',
+  component: Select,
+};
+
+export const Base = (args: SelectProps) => <Select {...args} />;
+
+Base.args = {
+  label: 'What is your name?',
+  placeholder: 'Panda',
+  onChange: action('Select changed'),
+  options: [
+    { label: 'Jane', value: 'jane' },
+    { label: 'John', value: 'john' },
+  ],
+};
---
+++ src/components/forms/Select/Select.tsx
@@ -0,0 +1,77 @@
+import React, { forwardRef, HTMLProps, Ref } from 'react';
+import cx from 'classnames';
+
+import { uniqueId } from '../../../util/unique-id';
+import { ReactComponent as Chevron } from '../../../icons/chevron-down.svg';
+import { Label } from '../Label';
+import { LabelBaseProps } from '../Label/Label';
+
+import styles from './Select.module.css';
+
+type Option = {
+  value: string | number;
+  label: string;
+  disabled: boolean;
+};
+
+export interface SelectProps
+  extends LabelBaseProps,
+    Omit<HTMLProps<HTMLSelectElement>, 'ref' | 'label'> {
+  placeholder: string;
+  options: Option[];
+  selectClassName?: string;
+}
+
+export const Select = forwardRef(
+  (
+    {
+      label,
+      id: customId,
+      value,
+      placeholder,
+      options,
+      disabled,
+      invalid,
+      validationHint,
+      className,
+      selectClassName,
+      ...props
+    }: SelectProps,
+    ref: Ref<HTMLSelectElement>,
+  ) => {
+    const id = customId || uniqueId();
+    return (
+      <Label
+        label={label}
+        htmlFor={id}
+        invalid={invalid}
+        disabled={disabled}
+        validationHint={validationHint}
+        className={className}
+      >
+        <select
+          ref={ref}
+          id={id}
+          aria-invalid={invalid}
+          disabled={disabled}
+          className={cx(styles.select, selectClassName)}
+          {...props}
+        >
+          {!value && (
+            <option key="placeholder" value="">
+              {placeholder}
+            </option>
+          )}
+          {options.map(({ label: labelValue, ...rest }) => (
+            <option key={rest.value} {...rest}>
+              {labelValue}
+            </option>
+          ))}
+        </select>
+        <Chevron className={styles.chevron} />
+      </Label>
+    );
+  },
+);
+
+Select.displayName = 'Select';
