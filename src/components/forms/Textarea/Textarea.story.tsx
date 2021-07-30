63703b6c6817f6a6285b450a61f8421937b2f496
Connor Bär <connor.baer@me.com>
2021-07-30 10:22:03 +0200

    Migrate Textarea component

---
+++ src/components/forms/Textarea/Textarea.story.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { Textarea, TextareaProps } from './Textarea';
+
+export default {
+  title: 'Forms/Textarea',
+  component: Textarea,
+};
+
+export const Base = (args: TextareaProps) => <Textarea {...args} />;
+
+Base.args = {
+  label: 'What is your name?',
+  placeholder: 'Panda',
+  onChange: action('Textarea changed'),
+};
---
+++ src/components/forms/Textarea/Textarea.tsx
@@ -0,0 +1,56 @@
+import React, { forwardRef, HTMLProps, Ref } from 'react';
+import cx from 'classnames';
+
+import { Label } from '../Label';
+import { LabelBaseProps } from '../Label/Label';
+import { uniqueId } from '../../../util/unique-id';
+
+import styles from './Textarea.module.css';
+
+export interface TextareaProps
+  extends LabelBaseProps,
+    Omit<HTMLProps<HTMLTextAreaElement>, 'ref' | 'label'> {
+  placeholder: string;
+  inputClassName?: string;
+}
+
+export const Textarea = forwardRef(
+  (
+    {
+      label,
+      id: customId,
+      disabled,
+      invalid,
+      className,
+      inputClassName,
+      validationHint,
+      suffix,
+      ...props
+    }: TextareaProps,
+    ref: Ref<HTMLTextAreaElement>,
+  ) => {
+    const id = customId || uniqueId();
+    return (
+      <Label
+        label={label}
+        htmlFor={id}
+        invalid={invalid}
+        disabled={disabled}
+        className={cx(styles.label, className)}
+        validationHint={validationHint}
+        suffix={suffix}
+      >
+        <textarea
+          ref={ref}
+          id={id}
+          aria-invalid={invalid}
+          disabled={disabled}
+          className={cx(styles.textarea, inputClassName)}
+          {...props}
+        />
+      </Label>
+    );
+  },
+);
+
+Textarea.displayName = 'Textarea';
