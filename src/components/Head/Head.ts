9bfa1333c5eccc82d294f27988d2639f5c5bdaa3
Connor Bär <connor.baer@me.com>
2021-08-07 19:18:47 +0200

    fix: adjust component types

--- src/components/Head/Head.ts
+++ src/components/Head/Head.ts
@@ -1,11 +1,11 @@
+import { HTMLProps, ReactNode, ReactPortal } from 'react';
 import { createPortal } from 'react-dom';
-import { HTMLProps, ReactPortal } from 'react';
 
 import { isServer } from '../../util/is-server';
 
 const headRoot = isServer ? null : document.head;
 
-export type HeadProps = HTMLProps<HTMLHeadElement>;
+export type HeadProps = HTMLProps<HTMLHeadElement> & { children: ReactNode };
 
 export function Head({ children }: HeadProps): ReactPortal | null {
   return headRoot ? createPortal(children, headRoot) : null;
--- src/components/typography/Link/Link.tsx
+++ src/components/typography/Link/Link.tsx
@@ -4,7 +4,10 @@ export type LinkProps<T> = T & {
   children: ReactElement<T>;
 };
 
-export function Link<T>({ children, ...props }: LinkProps<T>): ReactElement<T> {
+export function Link<T extends { href: string }>({
+  children,
+  ...props
+}: LinkProps<T>): ReactElement<T> {
   const child = Children.only(children);
   // @ts-expect-error I'm not sure where the conflicting type for `children`
   // is coming from. This works :shrug:
