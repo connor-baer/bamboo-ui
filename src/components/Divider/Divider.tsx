b67c36f875ef9b5cdbec098b1b14c1f2e0fb097d
Connor Bär <connor.baer@me.com>
2021-08-26 15:52:16 +0200

    Fix custom style overrides

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -9,5 +9,5 @@ export type DividerProps = HTMLProps<HTMLHRElement>;
  * A horizontal rule to visually and semantically separate text.
  */
 export function Divider(props: DividerProps): JSX.Element {
-  return <hr {...props} className={cx(props.className, styles.base)} />;
+  return <hr {...props} className={cx(styles.base, props.className)} />;
 }
